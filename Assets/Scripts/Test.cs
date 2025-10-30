77401ee055750f87a78e16e05aa7fab9209a5a3d
ljh0611 <a01077323633@gmail.com>
2025-10-30 14:49:46 +0900

    [Edit] 몇줄 수정하였음
    기존대비 초록줄은 추가 빨간줄은 삭제

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -15,6 +15,10 @@ public class Test : MonoBehaviour
     {
         
         Debug.Log ("안녕하세요");
+        Debug.Log ("지금 자다 일어나서 다시 강의 보면서 공부함");
+        Debug.Log("이렇게 하는게 맞냐? 죽것다");
+        //이것도 추가되
+
 
     }
 
