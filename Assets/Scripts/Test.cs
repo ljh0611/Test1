247e00b13ad0617587c4b9c9b55077c8d6ba7917
ljh0611 <a01077323633@gmail.com>
2025-10-30 14:25:27 +0900

    [Init] 최초 커밋
    수업을 위한 최초 커밋

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    public void Awake()
+    {
+        Debug.Log("여기다 쓰면 나오나");
+
+    }
+
+    void Start()
+    {
+        
+        Debug.Log ("안녕하세요");
+
+    }
+
+    
+}
